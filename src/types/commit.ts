abb32ff9640987ce2833e5d16ad41910e9d63229
Bao Do <baodo.dev@gmail.com>
2025-10-15 09:38:32 +0700

    Remove type commit file

--- src/types/commit.ts
+++
@@ -1,7 +0,0 @@
-export type Commit = {
-  date: string;        // 'YYYY-MM-DD'
-  message: string;
-  hours: number;       // thời gian đã dành ra
-  minutes: number;     // thời gian đã dành ra
-  time?: string;       // 'HH:MM' - thời điểm commit thực tế
-};
